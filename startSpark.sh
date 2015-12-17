a157707de68f7bb5917ed901b0addf98d2deebc1
Peterlee <peterlee0127@gmail.com>
2015-12-17 21:14:37 +0800

    update startSpark.sh

--- startSpark.sh
+++ startSpark.sh
@@ -7,7 +7,8 @@ filePath="$PWD"
 jarPath=$filePath/target/scala-$scalaVersion/SparkMain-assembly-$version.jar
 Class="SimpleApp"
 master=master
-
+mesos_master=master
+mesos_dispatcher=master
 
 if [  -z "$1"  ]
 then
@@ -24,17 +25,17 @@ fi
 if [ "$1" = "master" ]
 then
     echo "start at spark://192.168.1.70:7077"
-    spark-submit --class $Class --master spark://master:7077 $jarPath
+    spark-submit --class $Class --master spark://$master:7077 $jarPath
 fi
 
 if [ "$1" = "mesos" ]
 then
     echo "start at mesos://192.168.1.70:5050"
-    spark-submit --class $Class --master mesos://master:5050 $jarPath
+    spark-submit --class $Class --master mesos://$mesos_master:5050 $jarPath
 fi
 
 if [ "$1" = "mesos_cluster" ]
 then
     echo "start at mesos://dispatcher:7077"
-    spark-submit --class $Class --master mesos://dispatcher:7077 --deploy-mode cluster $jarPath
+    spark-submit --class $Class --master mesos://$mesos_dispatcher:7077 --deploy-mode cluster $jarPath
 fi
